75e1ca981d6d8712dea68d37070326089c5d1297
vladgrudak <ralkavladislav@gmail.com>
2020-05-15 13:34:58 +0300

    Initial commit

---
+++ WeekFinder/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  WeekFinder
+//
+//  Created by Владислав Ралка on 26.04.2020.
+//  Copyright © 2020 Grudak team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dayTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dayTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        
+        let calendar = Calendar.current
+        var dateComponents =  DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year  = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else { return }
+        
+        let weekDay = dateFormatter.string(from: date)
+        let capitalizedWeek = weekDay.capitalized
+        
+        resultLabel.text = capitalizedWeek
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
+ 
