ceb9d632c4584f3613bc23acb7a29502a4f18cf3
Vladislav Ralka <35633048+VladGrudak@users.noreply.github.com>
2020-05-20 01:28:30 +0300

    Update AppDelegate.swift

--- WeekFinder/AppDelegate.swift
+++ WeekFinder/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         print("Hello world!")
+        print("Test commit on github.com") 
         
         return true
     }
